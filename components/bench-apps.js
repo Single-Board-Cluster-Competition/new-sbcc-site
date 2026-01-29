06cddf5e5785071510a2a0178ebc8e207f8b473d
B1Bomber <97653406+B1Bomber@users.noreply.github.com>
2026-01-28 18:00:00 -0800

    Update bench-apps.js

--- components/bench-apps.js
+++ components/bench-apps.js
@@ -21,7 +21,52 @@ document.addEventListener('DOMContentLoaded', () => {
                         IQ-TREE
                     </summary>
                     <p>
-                        You need a high IQ to understand this tree. Your IQ is not high enough. 
+                        You need a high IQ to understand this tree. Your IQ is not high enough. Keep clicking...
+                        <br>
+                        Link: 
+                        <a href="https://github.com/iqtree/iqtree2">
+                            https://github.com/iqtree/iqtree2
+                        </a> 
+                    </p>
+                `;
+            }
+            else if (openCount <= 10 && openCount > 3) {
+                targetDetails.innerHTML = `
+                    <summary>
+                        IQ-TREE
+                    </summary>
+                    <p>
+                        Almost there...
+                        <br>
+                        Link: 
+                        <a href="https://github.com/iqtree/iqtree2">
+                            https://github.com/iqtree/iqtree2
+                        </a> 
+                    </p>
+                `;
+            }
+            else if (openCount <= 25 && openCount > 10) {
+                targetDetails.innerHTML = `
+                    <summary>
+                        IQ-TREE
+                    </summary>
+                    <p>
+                        You can do it! Keep clicking...
+                        <br>
+                        Link: 
+                        <a href="https://github.com/iqtree/iqtree2">
+                            https://github.com/iqtree/iqtree2
+                        </a> 
+                    </p>
+                `;
+            }
+            else if (openCount >= 45) {
+                targetDetails.innerHTML = `
+                    <summary>
+                        IQ-TREE
+                    </summary>
+                    <p>
+                        You need a high IQ to understand this tree. Your IQ is not high enough. Keep clicking...
                         <br>
                         Link: 
                         <a href="https://github.com/iqtree/iqtree2">
